8587277416a170b7fb0b438df2a0bfbbaa5ea615
nomad.ry@gmail.com <nomad.ry@gmail.com>
2014-03-28 17:08:27 -0600

    index wik contains sitemap to resolve human readable titles into page uri's

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var wik = {}
 
 
 
-
+wik.steward = require('./lib/steward.js')
 wik.create  = require('./lib/wikConstructor.js')
+wik.updater = require("./lib/wikUpdater.js")
 wik.factory = require('./lib/factory.js')
 wik.threadSpinner = require('./lib/threadSpinner.js')
 wik.resolveWik = require('./lib/wikResolver.js')
@@ -12,4 +13,7 @@ wik.diff = require('diff')
 wik.deepDiff = require('deep-diff')
 wik.nfd = require('NFD-js')
 wik.operator = require('substance-operator')
+
+
+wik.nfd.init({prefix: "wiki"}, function(){wik.steward.init(wik.updater.init);})
 module.exports = wik
